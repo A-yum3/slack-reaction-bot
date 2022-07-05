df258ab03d5700fd5cd2151b7ca42b4ad2aee5e1
ueyama-smv <y.ueyama@sonicmoov.com>
2022-07-06 00:06:41 +0900

    refactor(all): リファクタリングの実施

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { App, LogLevel } from '@slack/bolt';
+import AddReactionController from './controllers/AddReactionController';
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
@@ -9,56 +10,11 @@ const app = new App({
 });
 
 app.event('reaction_added', async ({ event, client }) => {
-    if (event.reaction !== 'meow_mudamudamuda') {
-        return;
-    }
-
-    if (event.item.type !== 'message') {
-        return;
-    }
-
-    const response = await client.emoji.list();
-
-    if (response.emoji === undefined) {
-        return;
-    }
-
-    const emoji_names = Object.keys(response.emoji);
-
-    const meows = emoji_names.filter((emoji_name) =>
-        emoji_name.includes('meow')
-    );
-
-    // const texts = emoji_names.filter((emoji_name) =>
-    //     emoji_name.includes('text')
-    // )
-
-    console.log(meows);
-
-    const channel = event.item.channel;
-    const ts = event.item.ts;
-
-    for (let i = 0; i < 10; i++) {
-        const index = Math.floor(Math.random() * meows.length);
-        const name = meows[index];
-        meows.splice(index, 1);
-        try {
-            await client.reactions.add({
-                name: name,
-                channel: channel,
-                timestamp: ts,
-            });
-        } catch (e) {
-            console.log('Skip because already added');
-        }
-
-    }
-
-    console.log(event);
+    const useCase = new AddReactionController(event, client);
+    await useCase.execute();
 });
 
 void (async () => {
     await app.start();
-
     console.log('⚡️ Bolt app is running!');
 })();
