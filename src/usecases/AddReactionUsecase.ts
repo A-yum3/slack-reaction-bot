c21e898ae3a27bf53c61b64fdbf39fa54bfcd52c
ueyama-smv <y.ueyama@sonicmoov.com>
2022-07-08 00:16:08 +0900

    fix(usecase): 拡張が簡単になるように修正

--- src/usecases/AddReactionUsecase.ts
+++ src/usecases/AddReactionUsecase.ts
@@ -5,57 +5,51 @@ import GetEmojiListRequestService from '../external/GetEmojiListRequestService';
 import AddReactionService from '../services/AddReactionService';
 
 export default class AddReactionUsecase {
-    private readonly extractReactionService: ExtractTargetReactionService;
-    private readonly client: WebClient;
-    private readonly channel: string;
-    private readonly timestamp: string;
+    private readonly _extractReactionService: ExtractTargetReactionService;
+    private readonly _client: WebClient;
+    private readonly _channel: string;
+    private readonly _timestamp: string;
 
     constructor(event: ReactionAddedEvent, client: WebClient) {
-        this.client = client;
+        this._client = client;
 
         if (event.item.type !== 'message') {
             throw new Error(`${event.item.type}は対象外のリアクションです。`);
         }
 
-        switch (event.reaction) {
-            case 'moriage_meow':
-                this.extractReactionService = new ExtractTargetReactionService(
-                    'meow',
-                    'meow'
-                );
-                break;
-            case 'moriage_praise':
-                this.extractReactionService = new ExtractTargetReactionService(
-                    'text',
-                    'praise'
-                );
-                break;
-            case 'moriage_welcome':
-                this.extractReactionService = new ExtractTargetReactionService(
-                    'text',
-                    'welcome'
-                );
-                break;
-            default:
-                throw new Error(
-                    `${event.reaction}は対象外のリアクションです。`
-                );
+        const result = event.reaction.match(/(^moriage)_(.*)/);
+
+        if (result === null) {
+            throw new Error(`${event.reaction}は対象外のリアクションです。`);
         }
-        this.channel = event.item.channel;
-        this.timestamp = event.item.ts;
+
+        let type = result[2];
+        const file_name = result[2];
+
+        if (type !== 'meow') {
+            type = 'text';
+        }
+
+        this._extractReactionService = new ExtractTargetReactionService(
+            type,
+            file_name
+        );
+
+        this._channel = event.item.channel;
+        this._timestamp = event.item.ts;
     }
 
     public async execute(): Promise<void> {
         const emoji_names = await GetEmojiListRequestService.execute(
-            this.client
+            this._client
         );
-        const target_emoji_names = await this.extractReactionService.execute(
+        const target_emoji_names = await this._extractReactionService.execute(
             emoji_names
         );
-        await new AddReactionService(this.client).execute(
+        await new AddReactionService(this._client).execute(
             target_emoji_names,
-            this.channel,
-            this.timestamp
+            this._channel,
+            this._timestamp
         );
     }
 }
